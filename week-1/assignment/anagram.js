90ed5897a29fc5ba655adc3b363919b561bd985d
pratik2630 <pratikkorgaonkar@gmail.com>
2024-06-11 16:27:12 +0530

    anagram code

---
+++ week-1/assignment/anagram.js
@@ -0,0 +1,17 @@
+function sort(str) {
+    var array = str.split("")
+    array = array.sort();
+    var sortedString = array.join("")
+    return sortedString
+}
+
+function isAnagram(str1,str2) {
+    if(sort(str1) == sort(str2)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var ans = isAnagram("hark","rakha");
+console.log(ans)
