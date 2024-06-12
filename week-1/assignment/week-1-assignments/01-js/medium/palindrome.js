e32fa6292f0d5e648ff57e5965c157f4751ab3b3
pratik2630 <pratikkorgaonkar@gmail.com>
2024-06-12 23:44:06 +0530

    palindrome code added

--- week-1/assignment/week-1-assignments/01-js/medium/palindrome.js
+++ week-1/assignment/week-1-assignments/01-js/medium/palindrome.js
@@ -5,9 +5,37 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
+function strReverse(str1){
+  var strRev = []
+  for (var i=str1.length-1;i>=0;i--){
+    strRev.push(str1[i])
+  }
+  return strRev
 
-function isPalindrome(str) {
-  return true;
 }
 
+function isPalindrome(str) {
+  // console.log(str)
+  newStr = str.toLowerCase()
+  console.log("String is converted to lowercase first !")
+  console.log("Input string: "+newStr)
+  //to reverse string manually by selfmade function
+  reversedStr = newStr.split('')
+  reversedStr = strReverse(reversedStr)
+  reversedStr = reversedStr.join('')
+
+  //by inbuilt function
+  // reversedStr = str.split('').reverse().join('')
+
+  console.log("reversed String :"+reversedStr)
+  if (newStr==reversedStr) {
+    return true;
+  } else {
+    return false;  
+  }
+  
+}
+
+ans = isPalindrome("bad")
+console.log(ans)
 module.exports = isPalindrome;
